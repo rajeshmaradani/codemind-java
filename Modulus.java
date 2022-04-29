cbfb23047c424784be4a07a30aeb874e135da021
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2022-04-29 23:58:58 +0530

    import java.util.Scanner; class avaarge {     public static void main(String args[])     {         int n,m,r;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            m=sc.nextInt();
            r=n%m;
            System.out.printf("%d",r);
        }
    }

---
+++ Modulus.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class avaarge
+{
+    public static void main(String args[])
+    {
+        int n,m,r;
+        
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        r=n%m;
+        System.out.printf("%d",r);
+    }
+}
