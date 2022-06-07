4d6162db6c4650fbf78a8eb45c5096ff8bb3e89e
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2022-06-07 22:22:54 +0530

    import java.util.Scanner; class Rajesh {     public static void main(String args[])     {         int n,r;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         r=n*(n-1)/2;         System.out.println(r);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Rajesh
+{
+    public static void main(String args[])
+    {
+        int n,r;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        r=n*(n-1)/2;
+        System.out.println(r);
+    }
+}
